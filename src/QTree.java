d4807239cdd62916184e7ad72ba6d624e77c16ef
Kevin <kjb2503@rit.edu>
2017-11-09 16:01:00 -0500

    Implemented rawFromCompressed, preorder, and toString methods.

--- src/QTree.java
+++ src/QTree.java
@@ -1,14 +1,14 @@
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
+import java.io.*;
 
 public class QTree
 {
+    public static int QUAD_SPLIT = -1;
+
     private int compressedSize;
     private int dim;
     private int [][] rawImage;
     private int rawSize;
     private FourZipNode root;
-    public static int QUAD_SPLIT = -1;
 
     public QTree()
     {
@@ -21,7 +21,7 @@ public class QTree
     }
 
 
-    public void compress()
+    public void compress() throws FourZipException
     {
 
     }
@@ -33,49 +33,51 @@ public class QTree
 
     public int getCompressedSize()
     {
-        return -1;
+        return compressedSize;
     }
 
     public int[][] getRawImage()
     {
-        return new int [1][1];
+        return rawImage;
     }
 
     public int getRawSize()
     {
-        return -1;
+        return rawSize;
     }
 
     public int getSideDim()
     {
-        return -1;
+        return dim;
     }
 
-    private static FourZipNode parse(BufferedReader file)
+    private static FourZipNode parse(BufferedReader file) throws IOException
     {
-        return null;
+        int line = Integer.parseInt(file.readLine());
+        return (line != -1) ? new FourZipNode(line) : new FourZipNode(parse(file), parse(file), parse(file), parse(file));
     }
 
     private String preorder(FourZipNode node)
     {
-        return "";
+        return (node.getValue() != -1) ? Integer.toString(node.getValue()) :
+                "(" + preorder(node.getChild(Quadrant.UL)) + ", " + preorder(node.getChild(Quadrant.UR)) + ", " +
+                        preorder(node.getChild(Quadrant.LL)) + ", " + preorder(node.getChild(Quadrant.LR)) + ")";
     }
 
-    @Override
-    public String toString()
+
+    public void uncompress() throws FourZipException
     {
-        return preorder(null);
+        // makes a new empty matrix
+        this.rawImage = new int[this.dim][this.dim];
     }
 
-
-    public void uncompress()
-    {
-
-    }
-
-    public void writeCompressed(String outputFile)
+    public void writeCompressed(String outputFile) throws IOException
     {
         FourZipNode node = new FourZipNode();
+        FileWriter fw = new FileWriter(outputFile);
+        BufferedWriter writer = new BufferedWriter(fw);
+
+        writeCompressed(null, writer);
     }
 
     private void writeCompressed(FourZipNode node, BufferedWriter writer)
@@ -83,13 +85,40 @@ public class QTree
 
     }
 
-    public static QTree rawFromFile(String filename)
+    public static QTree rawFromFile(String filename) throws IOException
     {
-        return null;
+        // initially makes an empty QTree
+        QTree tree = new QTree();
+
+        BufferedReader file = new BufferedReader(new FileReader(filename));
+
+        return tree;
     }
 
-    public static QTree compressedFromFile(String filename)
+    public static QTree compressedFromFile(String filename) throws IOException
     {
-        return null;
+        // initially makes an empty QTree
+        QTree tree = new QTree();
+
+        // constructs a new BufferedReader reading in the file
+        BufferedReader file = new BufferedReader(new FileReader(filename));
+
+        // the compressed size is the number of lines in the file
+        tree.rawSize = Integer.parseInt(file.readLine());
+        // the dimension is the square-root of the first line
+        tree.dim = (int)Math.round(Math.sqrt(tree.rawSize));
+
+        // set the root of the tree to be the return of the parse file on the remaining lines
+        tree.root = parse(file);
+
+        // return our newly generated tree
+        return tree;
+    }
+
+    @Override
+    public String toString()
+    {
+        // returns the preorder traversal of the root
+        return preorder(this.root);
     }
 }
